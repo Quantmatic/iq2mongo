bc5786ed79ded2ad24bad0c8644d2dc9d57d5d63
Quantmatic <quantomatic@outlook.com>
2017-04-16 20:16:01 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,19 @@
-import setuptools
-import os
+VERSION = '0.0.1'
 
-project_url = 'http://github.com/Quantmatic/iq2mongo/'
+import sys
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-except(IOError, ImportError):
-    if os.path.exists('README.md'):
-        long_description = open('README.md').read()
-    else:
-        long_description = 'IQFeed / DTN Data downloader. See details at: %s' % project_url
+from setuptools import setup
 
-setuptools.setup(name='iq2mongo',
-                 version='0.0.1',
-                 description='IQFeed / DTN Data downloader',
-                 long_description=long_description,
-                 classifiers=[
-                             'Development Status :: 1 - Beta',
-                             'License :: OSI Approved :: Apache Software License',
-                             'Programming Language :: Python :: 2.7',
-                             'Topic :: Office/Business :: Financial :: Investment',
+extra_args = {}
+if (sys.version_info[0] >= 3):
+    extra_args['use_2to3'] = True
 
-                 ],
-                 url=project_url,
-                 author='Alex Orion',
-                 author_email='',
-                 license='Apache License, Version 2.0',
-                 packages=setuptools.find_packages(),
-                 install_requires=['pandas', 'pymongo'],
-                 entry_points={'console_scripts': ['']},
-                 zip_safe=False)
+setup(name='iq2mongo',
+      version=VERSION,
+      description='iq2mongo',
+      author='Alex Orion',
+      url='https://github.com/quantmatic/iq2mongo/',
+      packages=['iq2mongo'],
+      install_requires=['pymongo',
+                        'pandas>=0.19'],
+      **extra_args)
